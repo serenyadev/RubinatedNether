3b85dc1411f3ac217d01934db6361a265bad7490
sery <sery@sappho.company>
2024-09-18 18:48:59 +0200

    datagen

--- fabric/src/main/java/net/artienia/rubinated_nether/fabric/client/RubinatedNetherFabricClient.java
+++ fabric/src/main/java/net/artienia/rubinated_nether/fabric/client/RubinatedNetherFabricClient.java
@@ -1,7 +1,10 @@
 package net.artienia.rubinated_nether.fabric.client;
 
+import net.artienia.rubinated_nether.block.ModBlocks;
 import net.artienia.rubinated_nether.client.RubinatedNetherClient;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.entity.monster.piglin.PiglinAi;
 
 public final class RubinatedNetherFabricClient implements ClientModInitializer {
@@ -9,5 +12,10 @@ public final class RubinatedNetherFabricClient implements ClientModInitializer {
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
         RubinatedNetherClient.clientSetup();
+
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MOLTEN_RUBY_GLASS.get(), RenderType.translucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MOLTEN_RUBY_GLASS_PANE.get(), RenderType.translucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUBY_GLASS.get(), RenderType.translucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUBY_GLASS_PANE.get(), RenderType.translucent());
     }
 }
